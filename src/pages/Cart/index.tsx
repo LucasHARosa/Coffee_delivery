4765eb3634f1505d4729b5d0fd9a378a51015ece
Lucas Rosa <l.ucas.henrique.a.r@gmail.com>
2023-04-08 11:10:15 -0300

    update

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,7 +1,20 @@
+import { ContainerData, ContainerFormAdress, ContainerFormPay, ContainerMain, ContainerPedido } from "./styles";
+
 export function Cart() {
   return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+    <ContainerMain>
+      <ContainerData>
+        <h3>Complete seu pedido</h3>
+        <ContainerFormAdress>
+          <p>Endereço de entrega</p>
+        </ContainerFormAdress>
+        <ContainerFormPay> 
+          <p>Forma de pagamento</p>
+        </ContainerFormPay>
+      </ContainerData>
+      <ContainerPedido>
+        <h3>Cafés selecionados</h3>
+      </ContainerPedido>
+    </ContainerMain>
   )
 }
---
+++ src/pages/Cart/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const ContainerMain = styled.main`
+  display: flex;
+`
+export const ContainerData = styled.section`
+  display: flex;
+
+`
+export const ContainerPedido = styled.section`
+  display: flex;
+`
+export const ContainerFormAdress = styled.div`
+  background-color: ${(props) => props.theme['base-card']};
+`
+export const ContainerFormPay = styled.div`
+  background-color: ${(props) => props.theme['base-card']};
+`
